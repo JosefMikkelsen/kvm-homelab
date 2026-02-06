2ec993891c4b5466051a91059d23ae05d6909cb6
Josef Assad <josef@josefassad.com>
2026-02-06 11:27:28 +0100

    set up ssh keys in guests and host

--- Makefile
+++ Makefile
@@ -11,6 +11,6 @@ undefine: shutdown
 	sudo virsh list --all --name | sudo xargs -r -n 1 virsh undefine --domain
 
 make remove-vols: undefine
-	for i in `yq -r .[2].block.[].loop.[].hostname roles/kvm_provision/tasks/main.yml`; do sudo rm /var/lib/libvirt/images/$$i.qcow2; done
+	for i in `yq -r .[].tasks.[].vars.machines[].hostname kvm_provision.yaml`; do sudo rm /var/lib/libvirt/images/$$i.qcow2; done
 
 make destroy: remove-vols
--- roles/kvm_provision/files/setup.sh
+++ roles/kvm_provision/files/setup.sh
@@ -3,3 +3,12 @@
 useradd josef --create-home --shell /bin/bash --user-group
 usermod -a -G sudo josef
 echo 'josef:hunter2' | chpasswd
+mkdir /home/josef/.ssh/
+chown josef:josef /home/josef/.ssh/
+chmod 0700 /home/josef/.ssh/
+cat /etc/homelab* > /home/josef/.ssh/authorized_keys
+chown josef:josef /home/josef/.ssh/authorized_keys
+chmod 0600 /home/josef/.ssh/authorized_keys
+rm /etc/homelab*
+ssh-keygen -A  # to generate ssh host keys, which are missing for some reason
+systemctl restart ssh
